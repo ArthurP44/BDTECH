c993b5bea14bb823b19f93a5d20326c0a46f57ae
Arthur Pralon <arthur.pralon@wanadoo.fr>
2020-05-06 11:38:14 +0200

    add margin top to site content div in base.html.twig

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,14 +8,7 @@ const $ = require('jquery');
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
 
-// or you can include specific pieces
-// require('bootstrap/js/dist/tooltip');
-// require('bootstrap/js/dist/popover');
-
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
-});
-
+// hide navbar on scroll down :
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
